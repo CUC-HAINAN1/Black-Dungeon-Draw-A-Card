5803fab77d724b70ccdcf1b9175f6ebd758cc250
mcaswen <mcaswen@gmail.com>
2025-03-27 18:25:09 +0800

    Modify the class name

--- Assets/Scripts/Player/DeductMana.cs
+++ Assets/Scripts/Player/DeductMana.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class DeductMana : MonoBehaviour {
 
    [Header("Deduct Setting")]
     public float interval = 2f;
     public int deductAmount = 10;
     private Coroutine _dudectCoroutine;
-
+    
     void Start()
     {
 
