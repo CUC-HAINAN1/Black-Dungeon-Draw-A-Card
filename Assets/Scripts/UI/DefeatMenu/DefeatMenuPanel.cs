21a95fc204eaf5bfd484aa2bd3cecb71a6320113
mcaswen <mcaswen@gmail.com>
2025-04-13 23:48:43 +0800

    feat: defeat UI scene

---
+++ Assets/Scripts/UI/DefeatMenu/DefeatMenuPanel.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathMenuPanel : MonoBehaviour {
+
+    [Header("UI 按钮")]
+    public Button TryAgainButton;
+    public Button ReturnButton;
+
+    private void Start() {
+
+        // 按钮绑定事件
+        TryAgainButton.onClick.AddListener(OnStartClicked);
+        ReturnButton.onClick.AddListener(OnReturnClicked);
+
+    }
+
+    private void OnStartClicked() {
+
+        Debug.Log("再次开始游戏！");
+        SceneTransitionHelper.Instance.LoadSceneWithTransition("SangTestScene");
+
+    }
+
+    private void OnReturnClicked() {
+
+        Debug.Log("回到主菜单！");
+        SceneTransitionHelper.Instance.LoadSceneWithTransition("MainMenuScene");
+
+    }
+}
