1455dc5df06e6193edfaac41260fc523c6028390
mcaswen <mcaswen@gmail.com>
2025-04-15 12:20:07 +0800

    feat: press O to reset card queue

--- Assets/Scripts/CardSystem/CardQueueSystem.cs
+++ Assets/Scripts/CardSystem/CardQueueSystem.cs
@@ -10,10 +10,10 @@ public class CardQueueSystem : MonoBehaviour {
 
     [Header("卡牌位置配置")]
     public Transform[] cardSlots = new Transform[4];
-    
+
     [System.Serializable]
     public class ActiveCardInfo {
-    
+
         public CardDataBase cardData;
         public GameObject cardInstance;
 
@@ -21,13 +21,13 @@ public class CardQueueSystem : MonoBehaviour {
 
     [Header("当前卡牌状态")]
     [SerializeField] public ActiveCardInfo[] currentCards = new ActiveCardInfo[4];
-    
+
     public int currentSlotIndex = 0;
     [SerializeField] public bool IsAnyCardDragging {get; private set;}
     [SerializeField] public bool IsAnyCardHovering {get; private set;}
 
     private void Awake() {
-        
+
         if (Instance != this && Instance != null) {
 
             Destroy(gameObject);
@@ -43,49 +43,59 @@ public class CardQueueSystem : MonoBehaviour {
 
     }
 
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.O)) {
+
+            IsAnyCardDragging = false;
+            IsAnyCardHovering = false;
+
+        }
+    }
+
     public void Initialize() {
-    
+
         ClearAllSlots();
-    
+
     }
 
     public CardDataBase DrawCard() {
-    
+
         return CardPoolManager.Instance.DrawCard();
-    
+
     }
 
     public void CreateCard(CardDataBase data, Transform targetSlot) {
-    
+
         int slotIndex = System.Array.IndexOf(cardSlots, targetSlot);
 
         ClearSlot(targetSlot);
 
         GameObject newCard = Instantiate(
-            
+
             cardPrefab,
             targetSlot.position,
             targetSlot.rotation,
             targetSlot
-        
+
         );
 
         newCard.transform.SetParent(targetSlot, false);
 
         currentCards[slotIndex] = new ActiveCardInfo {
-        
+
             cardData = data,
             cardInstance = newCard
-        
+
         };
 
         // 注册新卡牌状态
         CardStateManager.Instance.RegisterCard(newCard, false, false, false, data);
-        
+
     }
 
     public Transform GetNextSlot() {
-        
+
         int nextIndex = -1;
         for (int i = 0; i < currentCards.Length; i++) {
 
@@ -105,55 +115,55 @@ public class CardQueueSystem : MonoBehaviour {
     public bool TryUseCard(CardDragHandler draggedCard) {
 
         // 获取卡牌数据
-        int slotIndex = System.Array.FindIndex(currentCards, 
+        int slotIndex = System.Array.FindIndex(currentCards,
             c => c.cardInstance == draggedCard.gameObject);
 
-        if (slotIndex == -1 || 
-            PlayerAttributes.Instance.Mana < currentCards[slotIndex].cardData.manaCost) 
+        if (slotIndex == -1 ||
+            PlayerAttributes.Instance.Mana < currentCards[slotIndex].cardData.manaCost)
             return false;
-        
+
         PlayerAttributes.Instance.UseMana(currentCards[slotIndex].cardData.manaCost);
 
         // 执行卡牌使用逻辑
         ProcessCardUse(currentCards[slotIndex]);
         return true;
-    
+
     }
 
     private void ProcessCardUse(ActiveCardInfo usedCard) {
 
         CardStateManager.Instance.SetUsingState(usedCard.cardInstance, true);
-        
+
         var cardVisual = usedCard.cardInstance.GetComponent<CardVisual>();
         cardVisual.AnimateAndDestroy();
-        
+
         //CardStateManager.Instance.SetUsingState(usedCard.cardInstance, false);
         //ClearSlot(cardSlots[System.Array.IndexOf(currentCards, usedCard)]);
-        
+
     }
 
     private void ClearAllSlots() {
-    
+
         foreach(Transform slot in cardSlots) {
-        
+
             ClearSlot(slot);
-        
+
         }
     }
 
     private void ClearSlot(Transform slot) {
-        
+
         int slotIndex = System.Array.IndexOf(cardSlots, slot);
 
         if (slotIndex != -1) {
 
             foreach(Transform child in slot) {
-            
+
                 if (child.gameObject != null)
                     Destroy(child.gameObject);
 
             }
-        
+
         }
     }
 
@@ -170,15 +180,15 @@ public class CardQueueSystem : MonoBehaviour {
     }
 
     public void RefreshAllCardsHover() {
-        
+
         foreach (var card in currentCards) {
-            
+
             if (card?.cardInstance.gameObject == null) continue;
-            
+
             card.cardInstance.GetComponent<CardVisual>()?.CheckHoverAfterAnimation();
-        
+
         }
-    
+
     }
 
     [Header("基础配置")]
